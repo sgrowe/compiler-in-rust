f8919091df328aa2917e447bd3acd00dc8ef3267
sam <sgrowe@live.co.uk>
2021-05-04 20:49:20 +0100

    Update online playground

--- online-playground/src/index.js
+++ online-playground/src/index.js
@@ -1,42 +1,46 @@
 import init from "/online_playground.js"
 
-const wabt = window.WabtModule()
+async function main() {
+  const wabt = await window.WabtModule()
 
-const raf = window.requestAnimationFrame
+  const raf = window.requestAnimationFrame
 
-const output = document.getElementById("output")
-const logs = document.getElementById("logs")
-let prev_wat = ""
+  const output = document.getElementById("output")
+  const logs = document.getElementById("logs")
+  let prev_wat = ""
 
-async function log_output() {
-  raf(log_output)
+  async function log_output() {
+    raf(log_output)
 
-  const wat = output.innerText
+    const wat = output.innerText
 
-  if (wat === prev_wat) return
+    if (wat === prev_wat) return
 
-  prev_wat = wat
+    prev_wat = wat
 
-  try {
-    console.log("wabt", wabt)
-    console.log("wat", wat)
-    const module = wabt.parseWat("", wat, {})
-    const { buffer } = module.toBinary({})
+    try {
+      console.log("wabt", wabt)
+      console.log("wat", wat)
+      const module = wabt.parseWat("", wat, {})
+      const { buffer } = module.toBinary({})
 
-    module.destroy()
+      module.destroy()
 
-    const wasm_mod = await WebAssembly.instantiate(buffer)
-    console.log("wasm_mod", wasm_mod)
+      const wasm_mod = await WebAssembly.instantiate(buffer)
+      console.log("wasm_mod", wasm_mod)
 
-    logs.innerText = wasm_mod.instance.exports.main()
-  } catch (e) {
-    logs.innerText = `⚠️ Error running program: ${e}`
-    console.log("Run program error", e)
+      logs.innerText = wasm_mod.instance.exports.main()
+    } catch (e) {
+      logs.innerText = `⚠️ Error running program: ${e}`
+      console.log("Run program error", e)
+    }
   }
-}
 
-init().then(wasm => {
+  const wasm = await init()
+
   console.log("initialised", wasm)
 
   raf(log_output)
-})
+}
+
+main()
